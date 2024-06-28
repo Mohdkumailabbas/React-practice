1870bf008397af419e45cd5ae0e2497d46651401
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-06-28 14:30:17 +0530

    useState + conditional rendering

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
-function Card(){
-   const [val,setval]= useState({name:"card",isbannned:false});
-  //  const handlechange=function(){}
-  return(
-    <>
-    <h1 className='px-2 '> Name {val.name}</h1>
-    <h1 className='px-2'>bannned: {val.isbannned.toString()}</h1>
-   <button  onClick={() => setval({ ...val, isbannned: !val.isbannned})} className='bg-blue-400 rounded-md px-2 py-3'>Change</button>
-    </>
+import React, { useState } from "react";
+function Card() {
+  const [val, setval] = useState({ name: "Card", isbanned: false });
 
-  )
+  return (
+    <>
+      <h1 className="px-2">{val.name}</h1>
+      <h1 className="px-2">{val.isbanned.toString()}</h1>
+      <button
+        onClick={() => setval({ ...val, isbanned: !val.isbanned })}
+        className={`px-2 py-3 ${
+          val.isbanned ? "bg-blue-800" : "bg-red-300"
+        } rounded-md`}
+      >
+        chnge
+      </button>
+    </>
+  );
 }
 export default Card;
-    /* <button onClick={()=>setval((prev)=>prev+1)} className='bg-blue-400 rounded px-2 py-3'>change</button> */
+// const handelclick=()=>{
+//   setval(prevval =>({...prevval,age:prevval.age+1}))
+//  }
